9ca9ccbeac48803df67793a834c9f125b376a474
lozin <lozindenis6@gmail.com>
2022-10-15 09:36:59 +0300

    Create work "№2"

---
+++ main.py
@@ -0,0 +1,7 @@
+
+height = int(input('Введіть висоту квадрата - '))
+width = int(input('Введіть ширину квадрата - '))
+for i in range(height):
+    for j in range(width):
+        print('*', end="")
+    print()
