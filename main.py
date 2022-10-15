ada7ebcbaa863ce7c52801280799ad4188574b01
lozin <lozindenis6@gmail.com>
2022-10-15 10:46:41 +0300

    Create work "№3"

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+try:
+    size = int(input('Введіть розмір квадрата - '))
+    for i in range(size):
+        for j in range(size):
+            if i == 0 or j == 0 or i==size-1 or j==size-1:
+                sys.stdout.write("*")
+            else:
+                sys.stdout.write(" ")
+        print()
+except Exception as ex:
+    print(f'Eror information: {ex}')
